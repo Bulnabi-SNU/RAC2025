2f649258e2b98330356f1a883c55ed7ed2b4c1fc
chaewon <gbll0305@gmail.com>
2025-07-12 01:01:58 +0900

    1) modify README: how to run ros2 node with yaml file 2) modify name of python files.

--- src/vehicle_controller/setup.py
+++ src/vehicle_controller/setup.py
@@ -20,8 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'test_mc_01 = vehicle_controller.test_mc_01_takeoff_landing:main',
-            'test_mc_02 = vehicle_controller.test_mc_01_square:main',
+            'mc_test_01 = vehicle_controller.mc_test_01_takeoff_landing:main',
+            'mc_test_02 = vehicle_controller.mc_test_02_square:main',
         ],
     },
 )
