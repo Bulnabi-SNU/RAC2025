0c9a72faeeac4701d013602e0aba6efca8058449
chaewon <gbll0305@gmail.com>
2025-07-19 18:47:51 +0900

    update....ing px4_base_2.

--- src/vehicle_controller/vehicle_controller/mc_test_03_bezier.py
+++ src/vehicle_controller/vehicle_controller/mc_test_03_bezier.py
@@ -8,8 +8,8 @@ __contact__ = ""
 
 import rclpy
 from px4_msgs.msg import VehicleStatus, VehicleCommand
-from vehicle_controller.px4_base import PX4BaseController
-from vehicle_controller.bezier_handler import BezierCurve
+from vehicle_controller.code_basic.px4_base import PX4BaseController
+from vehicle_controller.code_basic.bezier_handler import BezierCurve
 
 import numpy as np
 import math
